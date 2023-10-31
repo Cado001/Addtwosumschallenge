eb9211cc8133163d2fbccdeb01fc9762face0cd1
Micado Pierre <Biml16gb@gmail.com>
2023-10-31 15:41:23 -0400

    Add Two Sum Challenge

--- challenge2.js
+++ challenge2.js
@@ -1,9 +1,11 @@
-
-
-exampleArray = [4, 8, 3, 10];
-
+// write a function that takes an array of
+// numbers and returns the largest number.
+exempleArray = [4, 8, 3, 10];
 function findLargest(arr){
-    for(let i = 0; i < arr.length; i++){
-
+    let largestSofar = arr[0] // we need to remember what the largest number we've seen is
+    for(let i=0; i < arr.length; i++){ // loop to let us look at each number
+        if(arr[i] > largestSofar){ // if the number we're looking at is larger
+            largestSofar = arr[i] // start keeping track of that number instead
+        }
     }
 }
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 
 
 
-
-function largerOfTwo(num1,num2,num3,num4){
-if(num1 > num2 && num1 > num3 && num1 > num4){
-
+function largerOfTwo( num1, num2){
+    if(num1> num2 ){
+        return num1 // return will end
+    } else{
+        return num2
+    }
 }
-if(num2 > num3 &&num2 > num4)
